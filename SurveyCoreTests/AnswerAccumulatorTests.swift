77e8863dda40cff5a8f8988d38c2c7c4e93dd9e0
eyup cimen <cmneyp@gmail.com>
2021-12-05 01:05:36 +0300

    t Remove more scaffolding

--- SurveyCoreTests/AnswerAccumulatorTests.swift
+++ SurveyCoreTests/AnswerAccumulatorTests.swift
@@ -15,15 +15,13 @@ class AnswerAccumulatorTests: XCTestCase {
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = AnswerAccumulator()
-        
     }
     
     override func tearDownWithError() throws {
         sut = nil
         try super.tearDownWithError()
     }
-
-
+ 
 //    func test_oneEntry() throws {
 //        let entry = WeightedEntry(weight: 8, problems: ["P1"], desires: [], killerFeatures: [])
 //        sut.accumulate(entry:entry)
@@ -38,27 +36,27 @@ class AnswerAccumulatorTests: XCTestCase {
 //        XCTAssertEqual(score, 5)
 //    }
     
-    func test_oneEntry_withDifferentProblem() throws {
-        let entry = WeightedEntry(weight: 5, problems: ["P1"], desires: [], killerFeatures: [])
-        
-        sut.accumulate(entry: entry)
-        let score: Int = sut.scoreProblem(problem: "P2")
-        
-        XCTAssertEqual(score, 0)
-    }
-    
-    func test_twoEntries() throws {
-        
-        let entry1 = WeightedEntry(weight: 8, problems: ["P1"], desires: [], killerFeatures: [])
-        let entry2 = WeightedEntry(weight: 5, problems: ["P1"], desires: [], killerFeatures: [])
-        
-        sut.accumulate(entry: entry1)
-        sut.accumulate(entry: entry2)
-        
-        let score: Int = sut.scoreProblem(problem: "P1")
-        
-        XCTAssertEqual(score, 8 + 5)
-    }
+//    func test_oneEntry_withDifferentProblem() throws {
+//        let entry = WeightedEntry(weight: 5, problems: ["P1"], desires: [], killerFeatures: [])
+//
+//        sut.accumulate(entry: entry)
+//        let score: Int = sut.scoreProblem(problem: "P2")
+//
+//        XCTAssertEqual(score, 0)
+//    }
+//
+//    func test_twoEntries() throws {
+//
+//        let entry1 = WeightedEntry(weight: 8, problems: ["P1"], desires: [], killerFeatures: [])
+//        let entry2 = WeightedEntry(weight: 5, problems: ["P1"], desires: [], killerFeatures: [])
+//
+//        sut.accumulate(entry: entry1)
+//        sut.accumulate(entry: entry2)
+//
+//        let score: Int = sut.scoreProblem(problem: "P1")
+//
+//        XCTAssertEqual(score, 8 + 5)
+//    }
     
     func test_twoEntries_withMultipleProblems() throws {
         
